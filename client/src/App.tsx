32a6d2e333d996f8853df1959f5c3589a73916f0
Jonas Henie <jonas.henie@nav.no>
2020-02-24 13:42:30 +0100

    Initial display css, add pdf

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,29 @@
+import './App.less';
+
 import React from 'react';
 
 import Navbar from './components/Navbar/Navbar';
 
-function App() {
+const App = () => {
     return (
         <>
             <Navbar />
-            <span style={{ display: 'flex', justifyContent: 'center' }}>
-                <div>asd</div>
-            </span>
+            <div className="content-container">
+                <div className="menu">menu</div>
+                <div className="form">form</div>
+                <div className="pdf-container">
+                    <object
+                        width="100%"
+                        height="100%"
+                        type="application/pdf"
+                        data="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+                    >
+                        Visning av sykmelding-pdf krever en plugin
+                    </object>
+                </div>
+            </div>
         </>
     );
-}
+};
 
 export default App;
