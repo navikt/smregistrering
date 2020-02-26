6ae21675c6010e5f7bb128ce6101fdb37eaa1698
Jonas Henie <jonas.henie@nav.no>
2020-02-26 10:46:02 +0100

    Refactor and type expandable sections

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -9,13 +9,26 @@ type FormProps = {
     sections: Sections;
 };
 
+type ExpandableSections =
+    | SectionTitle.MULIGHET_FOR_ARBEID
+    | SectionTitle.ARBEIDSEVNE
+    | SectionTitle.TIL_NAV
+    | SectionTitle.TIL_ARBEIDSGIVER;
+
 const Form = ({ sections }: FormProps) => {
-    const [expanded, setExpanded] = useState({
+    const [expanded, setExpanded] = useState<{ [key in ExpandableSections]: boolean }>({
         [SectionTitle.MULIGHET_FOR_ARBEID]: true,
         [SectionTitle.ARBEIDSEVNE]: true,
         [SectionTitle.TIL_NAV]: true,
         [SectionTitle.TIL_ARBEIDSGIVER]: true,
     });
+
+    const expandSection = (name: ExpandableSections) => {
+        setExpanded(state => ({
+            ...state,
+            [name]: !state[name],
+        }));
+    };
 
     return (
         <>
@@ -43,12 +56,7 @@ const Form = ({ sections }: FormProps) => {
                 <SectionContainer
                     section={sections[SectionTitle.MULIGHET_FOR_ARBEID]}
                     expanded={expanded[SectionTitle.MULIGHET_FOR_ARBEID]}
-                    setExpanded={() =>
-                        setExpanded(state => ({
-                            ...state,
-                            [SectionTitle.MULIGHET_FOR_ARBEID]: !state[SectionTitle.MULIGHET_FOR_ARBEID],
-                        }))
-                    }
+                    setExpanded={() => expandSection(SectionTitle.MULIGHET_FOR_ARBEID)}
                 >
                     <p>filler</p>
                     <p>filler</p>
@@ -62,12 +70,7 @@ const Form = ({ sections }: FormProps) => {
                 <SectionContainer
                     section={sections[SectionTitle.ARBEIDSEVNE]}
                     expanded={expanded[SectionTitle.ARBEIDSEVNE]}
-                    setExpanded={() =>
-                        setExpanded(state => ({
-                            ...state,
-                            [SectionTitle.ARBEIDSEVNE]: !state[SectionTitle.ARBEIDSEVNE],
-                        }))
-                    }
+                    setExpanded={() => expandSection(SectionTitle.ARBEIDSEVNE)}
                 >
                     <p>filler</p>
                     <p>filler</p>
@@ -76,12 +79,7 @@ const Form = ({ sections }: FormProps) => {
                 <SectionContainer
                     section={sections[SectionTitle.TIL_NAV]}
                     expanded={expanded[SectionTitle.TIL_NAV]}
-                    setExpanded={() =>
-                        setExpanded(state => ({
-                            ...state,
-                            [SectionTitle.TIL_NAV]: !state[SectionTitle.TIL_NAV],
-                        }))
-                    }
+                    setExpanded={() => expandSection(SectionTitle.TIL_NAV)}
                 >
                     <p>filler</p>
                     <p>filler</p>
@@ -90,12 +88,7 @@ const Form = ({ sections }: FormProps) => {
                 <SectionContainer
                     section={sections[SectionTitle.TIL_ARBEIDSGIVER]}
                     expanded={expanded[SectionTitle.TIL_ARBEIDSGIVER]}
-                    setExpanded={() =>
-                        setExpanded(state => ({
-                            ...state,
-                            [SectionTitle.TIL_ARBEIDSGIVER]: !state[SectionTitle.TIL_ARBEIDSGIVER],
-                        }))
-                    }
+                    setExpanded={() => expandSection(SectionTitle.TIL_ARBEIDSGIVER)}
                 >
                     <p>filler</p>
                     <p>filler</p>
