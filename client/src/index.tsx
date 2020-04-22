f18d12773cafe84df4013c64e22e0874d9b21ea1
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-04-22 11:20:40 +0200

    Fjerner service worker

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,17 +6,11 @@ import './index.less';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 import env from './utils/environment';
-
-ReactDOM.render(<App />, document.getElementById('root'));
 
 if (env.isDevelopment || env.isRunningOnHeroku) {
     require('./mock/setup');
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
