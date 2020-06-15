b55b729ae49bdfc393e02a1b42863b98cc81efaa
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-06-15 08:48:43 +0200

    oppdaterer mock

--- client/src/mock/setup.ts
+++ client/src/mock/setup.ts
@@ -1,14 +1,17 @@
 import FetchMock, { MiddlewareUtils } from 'yet-another-fetch-mock';
 
 import oppgave from './oppgave.json';
-import { RuleHitErrors } from '../types/RuleHitErrors';
+
+// Uncomment to use "invalid form" endpoint below
+// import { RuleHitErrors } from '../types/RuleHitErrors';
 
 const mock = FetchMock.configure({
     enableFallback: true, // default: true
     middleware: MiddlewareUtils.combine(MiddlewareUtils.delayMiddleware(1000), MiddlewareUtils.loggingMiddleware()), // default: (req, resp) => resp
 });
 
-const resBody: RuleHitErrors = {
+// Uncomment to use "invalid form" endpoint below
+/* const resBody: RuleHitErrors = {
     status: 'statusss',
     ruleHits: [
         {
@@ -18,10 +21,9 @@ const resBody: RuleHitErrors = {
             messageForUser: 'message for user',
         },
     ],
-};
+}; */
 
 mock.get('backend/api/v1/hentPapirSykmeldingManuellOppgave/', oppgave);
-mock.put('backend/api/v1/sendPapirSykmeldingManuellOppgave/', () => {
-    return Promise.resolve({ body: resBody, status: 400 });
-});
+mock.put('backend/api/v1/sendPapirSykmeldingManuellOppgave/', () => Promise.resolve({ status: 204 })); // For status ok
+// mock.put('backend/api/v1/sendPapirSykmeldingManuellOppgave/', () => Promise.resolve({ body: resBody, status: 400 })); // For invalid form response. Errors returned in body. TODO: Something wrong with the mock, works in dev.
 mock.get('/user', 'Testbruker');
