0fc08cd6afaa263eb953b72338c20abbdd762d20
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-06-18 11:53:12 +0200

    tester for string encoding

--- client/src/__tests__/CustomTypes.test.ts
+++ client/src/__tests__/CustomTypes.test.ts
@@ -1,6 +1,7 @@
+import * as iots from 'io-ts';
 import * as iotsPromise from 'io-ts-promise';
 
-import { createEnumType } from '../types/CustomTypes';
+import { DateFromString, DateTimeFromString, createEnumType } from '../types/CustomTypes';
 
 describe('CustomTypes', () => {
     describe('EnumType', () => {
@@ -17,6 +18,22 @@ describe('CustomTypes', () => {
         });
         it('Should encode enum to the original string', () => {
             expect(enumType.encode(TestEnum.APPLE)).toBe('APPLE');
+        });
+    });
+
+    describe('DateFromString', () => {
+        it('Should encode to correct date-string', () => {
+            const dateString = '2019-06-10';
+            const date = new Date(dateString);
+            expect(DateFromString.encode(date)).toBe(dateString);
+        });
+    });
+
+    describe('DateTimeFromString', () => {
+        it('Should encode to correct datetime-string', () => {
+            const dateString = '2018-11-29T23:00:00.000Z';
+            const date = new Date(dateString);
+            expect(DateTimeFromString.encode(date)).toBe(dateString);
         });
     });
 });
