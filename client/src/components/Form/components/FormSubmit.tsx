bcf1f044794f4a294a315a2914c0028da795ad8c
Sebastian <sebastian.knudsen@nav.no>
2020-11-06 10:32:02 +0100

    Move avvis and send to gosys buttons from bottom to side menu (#101)
    * Move avvis and send to gosys buttons from bottom to side menu
    
    * make scrollbar in side menu hide automatically
    
    * Add tests for avvis and send back to gosys
    
    * Fix typo and expand viewport in cypress tests
    
    * audit packed
    
    * Make tests run off of single script
    
    * Build with same node version as build/test server because tests are failing at build while working locally
    
    * Change integration test starting script for build process because process must be able to terminate for github actions to proceed
    
    Co-authored-by: Sebastian Knudsen <sebastianknudsen@Sebastians-MacBook-Pro.local>

--- client/src/components/Form/components/FormSubmit.tsx
+++ client/src/components/Form/components/FormSubmit.tsx
@@ -5,8 +5,8 @@ import Modal from 'nav-frontend-modal';
 import React, { useState } from 'react';
 import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 import { Checkbox } from 'nav-frontend-skjema';
-import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
-import { Fareknapp, Flatknapp, Hovedknapp } from 'nav-frontend-knapper';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import { Hovedknapp } from 'nav-frontend-knapper';
 
 import { Oppgave } from '../../../types/Oppgave';
 import { RegistrertSykmelding } from '../../../types/RegistrertSykmelding';
@@ -25,20 +25,10 @@ interface FormSubmitProps {
 const FormSubmit = ({ oppgave, schema, validateAll, errorSummaryRef, enhet }: FormSubmitProps) => {
     // Registrer sykmelding
     const [checked, setChecked] = useState<boolean>(false);
-    const [isLoadingSuccess, setIsLoadingSuccess] = useState<boolean>(false);
     const [ruleHitErrors, setRuleHitErrors] = useState<RuleHitErrors | undefined>(undefined);
+    const [isLoadingSuccess, setIsLoadingSuccess] = useState<boolean>(false);
     const [successModalContent, setSuccessModalContent] = useState<string | undefined>(undefined);
     const [successError, setSuccessError] = useState<Error | null>(null);
-
-    // Avvis sykmelding
-    const [rejectModalOpen, setRejectModalOpen] = useState<boolean>(false);
-    const [isLoadingReject, setIsLoadingReject] = useState<boolean>(false);
-    const [rejectError, setRejectError] = useState<Error | null>(null);
-
-    // Reverter til GOSYS
-    const [revertModalOpen, setRevertModalOpen] = useState<boolean>(false);
-    const [isLoadingRevert, setIsLoadingRevert] = useState<boolean>(false);
-    const [revertError, setRevertError] = useState<Error | null>(null);
 
     Modal.setAppElement('#root');
 
@@ -59,7 +49,7 @@ const FormSubmit = ({ oppgave, schema, validateAll, errorSummaryRef, enhet }: Fo
                 })
                     .then((res) => {
                         if (res.status === 204) {
-                            setSuccessModalContent("Oppgaven ble ferdigstilt.");
+                            setSuccessModalContent('Oppgaven ble ferdigstilt.');
                         } else if (res.status === 400) {
                             return res.json();
                         } else {
@@ -89,72 +79,6 @@ const FormSubmit = ({ oppgave, schema, validateAll, errorSummaryRef, enhet }: Fo
                     errorSummaryRef.current.scrollIntoView({ behavior: 'smooth' });
                 }
             }, 300);
-        }
-    };
-
-    const rejectSykmelding = () => {
-        if (!enhet) {
-            setRejectError(new Error('Enhet mangler. Vennligst velg enhet øverst på siden'));
-        } else {
-            setIsLoadingReject(true);
-            setRejectError(null);
-            fetch(`backend/api/v1/oppgave/${oppgave.oppgaveid}/avvis`, {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Nav-Enhet': enhet,
-                },
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        setRejectModalOpen(false);
-                        setSuccessModalContent("Oppgaven ble ferdigstilt.");
-                    } else {
-                        throw new Error(
-                            `En feil oppsto ved avvisning av oppgave: ${oppgave.oppgaveid}. Feilkode: ${response.status}`,
-                        );
-                    }
-                })
-                .catch((error) => {
-                    setRejectError(error);
-                })
-                .finally(() => {
-                    setIsLoadingReject(false);
-                });
-        }
-    };
-
-    const revertSykmelding = () => {
-        if (!enhet) {
-            setRevertError(new Error('Enhet mangler. Vennligst velg enhet øverst på siden'));
-        } else {
-            setIsLoadingRevert(true);
-            setRevertError(null);
-            fetch(`backend/api/v1/oppgave/${oppgave.oppgaveid}/tilgosys`, {
-                method: 'POST',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Nav-Enhet': enhet,
-                },
-            })
-                .then((response) => {
-                    if (response.ok) {
-                        setRevertModalOpen(false);
-                        setSuccessModalContent("Oppgaven ble sendt tilbake til GOSYS.");
-                    } else {
-                        throw new Error(
-                            `En feil oppsto ved sending av oppgave til GOSYS: ${oppgave.oppgaveid}. Feilkode: ${response.status}`,
-                        );
-                    }
-                })
-                .catch((error) => {
-                    setRevertError(error);
-                })
-                .finally(() => {
-                    setIsLoadingRevert(false);
-                });
         }
     };
 
@@ -207,72 +131,17 @@ const FormSubmit = ({ oppgave, schema, validateAll, errorSummaryRef, enhet }: Fo
             >
                 Registrer sykmelding
             </Hovedknapp>
-            <Flatknapp htmlType="button" onClick={() => setRejectModalOpen(true)}>
-                Avvis sykmelding
-            </Flatknapp>
-
-            <Flatknapp htmlType="button" onClick={() => setRevertModalOpen(true)}>
-                Send oppgave tilbake til GOSYS
-            </Flatknapp>
-
             <Modal
                 isOpen={!!successModalContent}
                 onRequestClose={() => setSuccessModalContent(undefined)}
                 closeButton
-                contentLabel={successModalContent || ''}
+                contentLabel={successModalContent || ''}
             >
                 <div style={{ display: 'flex', flexDirection: 'column', padding: '2rem 2.5rem' }}>
                     <Normaltekst style={{ marginBottom: '2rem' }}>{successModalContent}</Normaltekst>
                     <a href={process.env.REACT_APP_GOSYS_URL} tabIndex={0} className="knapp knapp--hoved">
                         Tilbake til GOSYS
                     </a>
-                </div>
-            </Modal>
-            <Modal
-                isOpen={rejectModalOpen}
-                onRequestClose={() => setRejectModalOpen(false)}
-                closeButton
-                contentLabel="Bekreft avvisning av sykmelding"
-            >
-                <div className="cancelmodal">
-                    <Undertittel tag="h1" className="cancelmodal--title">
-                        Er du sikker på at du vil avvise sykmeldingen?
-                    </Undertittel>
-                    <Normaltekst tag="p" className="cancelmodal--content">
-                        Dette vil ferdigstille oppgaven. Sykmeldingen blir ikke registrert i infotrygd. Behandler og
-                        pasient blir ikke varslet.
-                    </Normaltekst>
-                    <Fareknapp
-                        className="cancelmodal--button"
-                        spinner={isLoadingReject}
-                        onClick={() => rejectSykmelding()}
-                    >
-                        AVVIS SYKMELDING
-                    </Fareknapp>
-                    {rejectError && <AlertStripeFeil>{rejectError.message}</AlertStripeFeil>}
-                </div>
-            </Modal>
-            <Modal
-                isOpen={revertModalOpen}
-                onRequestClose={() => setRevertModalOpen(false)}
-                closeButton
-                contentLabel="Send sykmelding tilbake til GOSYS"
-            >
-            <div className="cancelmodal">
-                <Undertittel tag="h1" className="cancelmodal--title">
-                        Er du sikker på at du vil sende oppgaven tilbake til GOSYS?
-                    </Undertittel>
-                    <Normaltekst tag="p" className="cancelmodal--content">
-                        Dette vil ikke ferdigstille oppgaven, men gjør det mulig å behandle den i GOSYS.
-                    </Normaltekst>
-                    <Fareknapp
-                        className="cancelmodal--button"
-                        spinner={isLoadingRevert}
-                        onClick={() => revertSykmelding()}
-                    >
-                        Send til GOSYS
-                    </Fareknapp>
-                    {revertError && <AlertStripeFeil>{revertError.message}</AlertStripeFeil>}
                 </div>
             </Modal>
         </div>
