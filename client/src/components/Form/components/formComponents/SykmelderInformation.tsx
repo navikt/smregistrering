25f2b0eb1f620a08a6d539c1d4b49a9e5b6d38c6
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-11-20 14:51:16 +0100

    Started work on presenting behandler information

--- client/src/components/Form/components/formComponents/SykmelderInformation.tsx
+++ client/src/components/Form/components/formComponents/SykmelderInformation.tsx
@@ -1,9 +1,11 @@
+import 'nav-frontend-tabell-style';
+
 import './SykmelderInformation.less';
 
 import React from 'react';
 import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
-import { Sykmelder } from '../../../../types/Sykmelder';
+import { Helsepersonellkategori, Sykmelder } from '../../../../types/Sykmelder';
 
 interface SykmelderInformationProps {
     sykmelder: Sykmelder | null | undefined;
@@ -19,22 +21,61 @@ const SykmelderInformation = ({ sykmelder }: SykmelderInformationProps) => {
             <header>
                 <Undertittel tag="h3">Informasjon om behandler</Undertittel>
             </header>
-            <section>
-                <Element tag="h4">Fødselsnummer:</Element>
-                <Normaltekst>{sykmelder.fnr}</Normaltekst>
-            </section>
-            <section>
-                <Element tag="h4">Aktør ID</Element>
-                <Normaltekst>{sykmelder.aktorId}</Normaltekst>
-            </section>
-            {sykmelder.fornavn ? (
-                <section>
-                    <Element tag="h4">Navn:</Element>
-                    <Normaltekst>
-                        {sykmelder.fornavn} {sykmelder.mellomnavn} {sykmelder.etternavn}
-                    </Normaltekst>
+            <div className="sykmelder-information__content">
+                <div className="sykmelder-information__col">
+                    {sykmelder.fornavn ? (
+                        <section>
+                            <Element tag="h4">Navn:</Element>
+                            <Normaltekst>
+                                {sykmelder.fornavn} {sykmelder.mellomnavn} {sykmelder.etternavn}
+                            </Normaltekst>
+                        </section>
+                    ) : null}
+                    <section>
+                        <Element tag="h4">Fødselsnummer:</Element>
+                        <Normaltekst>{sykmelder.fnr}</Normaltekst>
+                    </section>
+                </div>
+                <section className="sykmelder-information__col">
+                    <Element tag="h4">Godkjenninger:</Element>
+                    {sykmelder.godkjenninger.length > 0 ? (
+                        <table className="tabell">
+                            <thead>
+                                <tr>
+                                    <th>Kategori</th>
+                                    <th>Aktiv</th>
+                                    <th>Godkjenningstype</th>
+                                    <th>Rett til å sykmelde</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {sykmelder.godkjenninger.map((godkjenning) => {
+                                    if (godkjenning.helsepersonellkategori?.verdi) {
+                                        return (
+                                            <>
+                                                <tr>
+                                                    <td>
+                                                        {
+                                                            Helsepersonellkategori[
+                                                                godkjenning.helsepersonellkategori.verdi
+                                                            ]
+                                                        }
+                                                    </td>
+                                                    <td>{godkjenning.helsepersonellkategori?.aktiv ? 'Ja' : 'Nei'}</td>
+                                                    <td>Student</td>
+                                                    <td>Ja</td>
+                                                </tr>
+                                            </>
+                                        );
+                                    }
+                                })}
+                            </tbody>
+                        </table>
+                    ) : (
+                        <Normaltekst>Behandler mangler godkjenninger</Normaltekst>
+                    )}
                 </section>
-            ) : null}
+            </div>
         </article>
     );
 };
--- client/src/types/Sykmelder.ts
+++ client/src/types/Sykmelder.ts
@@ -1,10 +1,72 @@
 import * as iots from 'io-ts';
 
+export enum Helsepersonellkategori {
+    'AA' = 'Ambulansearbeider',
+    'AT' = 'ApotekteknikerA',
+    'AU' = 'udiograf',
+    'BI' = 'Bioingeniør',
+    'ET' = 'Ergoterapeut',
+    'FA1' = 'Provisorfarmasøyt',
+    'FA2' = 'Reseptarfarmasøyt',
+    'FB' = 'Fiskehelsebiolog',
+    'FO' = 'Fotterapeut',
+    'FT' = 'Fysioterapeut',
+    'HE' = 'Helsesekretær',
+    'HF' = 'Helsefagarbeider',
+    'HP' = 'Hjelpepleier',
+    'JO' = 'Jordmor',
+    'KE' = 'Klinisk ernæringsfysiolog',
+    'KI' = 'Kiropraktor',
+    'LE' = 'Lege',
+    'OA' = 'Omsorgsarbeider',
+    'OI' = 'Ortopediingeniør',
+    'OP' = 'Optiker',
+    'OR' = 'Ortoptist',
+    'PE' = 'Perfusjonist',
+    'PS' = 'Psykolog',
+    'RA' = 'Radiograf',
+    'SP' = 'Sykepleier',
+    'TH' = 'Tannhelsesekretær',
+    'TL' = 'Tannlege',
+    'TP' = 'Tannpleier',
+    'TT' = 'Tanntekniker',
+    'VE' = 'Veterinær',
+    'VP' = 'Vernepleier',
+    'XX' = 'Ukjent/uspesifisert',
+    'MT' = 'Manuellterapeut',
+}
+
+const HelsepersonellkategoriKode = iots.intersection([
+    iots.type({
+        aktiv: iots.boolean,
+        oid: iots.number,
+    }),
+    iots.partial({
+        verdi: iots.union([iots.keyof(Helsepersonellkategori), iots.null]),
+    }),
+]);
+
+const AutorisasjonKode = iots.intersection([
+    iots.type({
+        aktiv: iots.boolean,
+        oid: iots.number,
+    }),
+    iots.partial({
+        verdi: iots.union([iots.string, iots.null]),
+    }),
+]);
+
+const Godkjenning = iots.partial({
+    helsepersonellkategori: iots.union([HelsepersonellkategoriKode, iots.null]),
+    autorisasjon: iots.union([AutorisasjonKode, iots.null]),
+});
+
 export const Sykmelder = iots.intersection([
     iots.type({
         hprNummer: iots.string,
         fnr: iots.string,
         aktorId: iots.string,
+        godkjenninger: iots.array(Godkjenning),
     }),
     iots.partial({
         fornavn: iots.union([iots.string, iots.null]),
