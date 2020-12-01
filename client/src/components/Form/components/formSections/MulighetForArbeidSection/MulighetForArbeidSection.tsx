dee39dca87077c59ca7917e920f51550440ab00f
Jonas Henie <jonas.henie@nav.no>
2020-12-01 13:18:45 +0100

    Update add period button style

--- client/src/components/Form/components/formSections/MulighetForArbeidSection/MulighetForArbeidSection.tsx
+++ client/src/components/Form/components/formSections/MulighetForArbeidSection/MulighetForArbeidSection.tsx
@@ -3,6 +3,7 @@ import './MulighetForArbeidSection.less';
 import React from 'react';
 import { Element } from 'nav-frontend-typografi';
 import { FeiloppsummeringFeil, Select } from 'nav-frontend-skjema';
+import { Knapp } from 'nav-frontend-knapper';
 
 import ClearButton from '../../formComponents/ClearButton';
 import SectionContainer from '../../SectionContainer';
@@ -218,9 +219,9 @@ const MulighetForArbeidSection = ({ section, setFormState, formState, errors }: 
                     </div>
                 ))}
 
-            <button
-                type="button"
-                className="mulighetForArbeid__add"
+            <Knapp
+                htmlType="button"
+                form="kompakt"
                 onClick={(event) => {
                     event.preventDefault();
                     setFormState(
@@ -241,17 +242,8 @@ const MulighetForArbeidSection = ({ section, setFormState, formState, errors }: 
                     );
                 }}
             >
-                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M16.3717 2.82C14.565 1.00583 12.1617 0.005 9.58252 0C4.30835 0 0.0100188 4.29 1.88156e-05 9.56417C-0.00498118 12.1242 0.986685 14.5325 2.79335 16.3467C4.60002 18.16 7.00418 19.1617 9.56502 19.1667H9.58335C14.8567 19.1667 19.1559 14.8758 19.1659 9.60083C19.1709 7.04167 18.1784 4.63333 16.3717 2.82ZM14.5834 10H10V14.5833C10 14.8133 9.81252 15 9.58335 15C9.35336 15 9.16669 14.8133 9.16669 14.5833V10H4.58336C4.35336 9.99917 4.16586 9.81333 4.16669 9.58333C4.16669 9.35417 4.35336 9.16667 4.58336 9.16667H9.16669V4.58333C9.16669 4.35333 9.35336 4.16667 9.58335 4.16667C9.81252 4.16667 10 4.35333 10 4.58333V9.16667H14.5834C14.8125 9.16667 15.0009 9.35333 15 9.58333C15 9.81333 14.8134 10 14.5834 10Z"
-                        fill="#0067C5"
-                    />
-                </svg>
-
-                <span className="mulighetForArbeid__add-text">Legg til periodetype</span>
-            </button>
+                Legg til periode
+            </Knapp>
         </SectionContainer>
     );
 };
