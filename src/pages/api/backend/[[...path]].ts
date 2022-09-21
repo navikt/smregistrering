062330dd2fc93f596694281135747f67cf33901c
Karl O <k@rl.run>
2022-09-21 09:51:22 +0100

    use @navikt/next-api-proxy (#218)

--- src/pages/api/backend/[[...path]].ts
+++ src/pages/api/backend/[[...path]].ts
@@ -1,9 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { proxiedApiRouteConfig, proxyApiRouteRequest } from '@navikt/next-api-proxy';
 import { logger } from '@navikt/next-logger';
 
 import { withAuthenticatedApi } from '../../../auth/withAuth';
-import { proxyRequest } from '../../../proxy/proxy-backend';
 import { getServerEnv, isLocalOrDemo } from '../../../utils/env';
+import { getAzureAdAccessToken } from '../../../auth/azureTokens';
 
 const allowedAPIs = [
     'GET /api/v1/pasient',
@@ -16,27 +16,41 @@ const allowedAPIs = [
     'POST /api/v1/sykmelding/[uuid]',
 ];
 
-const handler = withAuthenticatedApi(async (req: NextApiRequest, res: NextApiResponse) => {
+const handler = withAuthenticatedApi(async (req, res, accessToken) => {
     if (isLocalOrDemo) {
         logger.info('Skipping setting up proxy for local or demo');
         return;
     }
 
-    await proxyRequest({
-        req: req,
-        res: res,
-        allowedAPIs: allowedAPIs,
-        backend: 'backend',
-        backendHostname: getServerEnv('SMREGISTRERING_BACKEND_HOST'),
-        backendClientId: getServerEnv('SMREGISTRERING_BACKEND_SCOPE'),
+    const rewrittenPath = req.url!.replace(`/api/backend`, '');
+    const api = `${req.method} ${rewrittenPath}`;
+    if (!allowedAPIs.includes(cleanPath(api))) {
+        logger.warn(`404 Unknown API: ${api}, clean path: ${cleanPath(api)}`);
+        res.status(404);
+        res.send(null);
+        return;
+    }
+
+    const bearerToken = await getAzureAdAccessToken(accessToken, getServerEnv('SMREGISTRERING_BACKEND_SCOPE'));
+
+    logger.info(`Proxying request for path ${getServerEnv('SMREGISTRERING_BACKEND_HOST')}${rewrittenPath}`);
+    await proxyApiRouteRequest({
+        path: rewrittenPath,
+        req,
+        res,
+        bearerToken,
+        hostname: getServerEnv('SMREGISTRERING_BACKEND_HOST'),
+        https: false,
     });
 });
 
-export const config = {
-    api: {
-        bodyParser: false,
-        externalResolver: true,
-    },
-};
+const UUID = /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/g;
+const OPPGAVE_OR_HPR = /[0-9]{7,9}/;
+
+export function cleanPath(value: string): string {
+    return value?.replace(UUID, '[uuid]').replace(OPPGAVE_OR_HPR, '[id|hpr]');
+}
+
+export const config = proxiedApiRouteConfig;
 
 export default handler;
